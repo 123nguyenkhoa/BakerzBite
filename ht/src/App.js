6813754b73c70f3bf8abd5cb5df045f7c70f82cc
huy04082000 <huy04082000@gmail.com>
2024-06-22 10:40:22 +0700

    test

--- ht/src/App.js
+++ ht/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './components/Navbar';
+import HeroSlider from './components/HeroSlider';
+import Services from './components/Services';
+import About from './components/About';
+import SpecialDish from './components/SpecialDish';
+import Menu from './components/Menu';
+import Testimonials from './components/Testimonials';
+import Reservation from './components/Reservation';
+import Features from './components/Features';
+import Event from './components/Event';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <HeroSlider />
+      <Services />
+      <About />
+      <SpecialDish />
+      <Menu />
+      <Testimonials />
+      <Reservation />
+      <Features />
+      <Event />
+      <Footer />
     </div>
   );
 }
